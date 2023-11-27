40499239169f42f76440304db004c4a618e6a3e8
Utente <Utente@PC-DI-SIMO>
2023-11-27 16:56:18 +0100

    Aggiungere i file di progetto.

---
+++ prog a oggetti 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog_a_oggetti_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrato q = new Quadrato();    
+            q.Lato = float.Parse(Console.ReadLine());
+            Console.WriteLine("{0}", q.Lato * q.Lato);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ prog a oggetti 1/Quadrato.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog_a_oggetti_1
+{
+    internal class Quadrato
+    {
+        public float Lato { get; set; }
+    }
+}
